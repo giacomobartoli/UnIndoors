81589351471bc42d834d5c23eda6cf637b3daded
GiacomoOracolo <gzano93@gmail.com>
2017-11-22 18:40:36 +0100

    features added
    added svg, collapse option, and reorganized layouts

---
+++ js/loginpage.js
@@ -0,0 +1,21 @@
+/*global $ */
+
+function onImageHover() {
+    "use strict";
+
+    $("#business_man").hover(
+
+        function () {
+
+            $("business_man").collapse();
+        }, 
+        function () {
+
+
+        }
+
+
+
+    );
+
+}
