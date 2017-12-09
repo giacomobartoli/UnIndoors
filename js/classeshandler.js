be407ff3a2b74af41a844f1e8790145ac01d1640
GiacomoOracolo <gzano93@gmail.com>
2017-12-09 16:54:59 +0100

    modified and solved bugs

--- js/classeshandler.js
+++ js/classeshandler.js
@@ -16,6 +16,8 @@ var database = firebase.database();
 //var carouselInnerActiveElementRow=document.getElementById('row_active');
 var carouselInnerElementToday=document.getElementById('today_classes_inner');
 var carouselOuterElementToday=document.getElementById('carousel_today_classes');
+var carouselInnerElementWeek=document.getElementById('week_classes_inner');
+var carouselOuterElementWeek=document.getElementById('carousel_week_classes')
 var carouselNotActiveItemNumber=0;
 var rowIDNumber=0;
 var todayClassesCarouselSlide=document.getElementById('carouselExampleControls');
@@ -28,40 +30,98 @@ var todayClassesCarouselSlide=document.getElementById('carouselExampleControls')
 function setUpTodayClasses(size) {
     var date = new Date();
     var today = date.getDay();
-    var classes=[];
+    var lessons=[]
 
-    database.ref('CesenaCampus/Corsi/').once('value').then(function (snapshot) {
+    database.ref('CesenaCampus/Corsi/').on('value',function (snapshot) {
+
+
         snapshot.forEach(function (childSnapshot) {
             var lessonName=childSnapshot.child('name').val().toString();
+            console.log(lessonName,snapshot.numChildren())
 
             childSnapshot.child('schedule').ref.once('value').then(function (scheduleSnapshot) {
+                var dayoflessons=[]
                 scheduleSnapshot.forEach(function (daySnapshot) {
+                    //  console.log('array size ',dayoflessons.length)
+                    if(lessonName=='Business Intelligence'){
+                        console.log(daySnapshot.val())
+                    }
                     var dayNumber = daySnapshot.child('value').val();
-                    if (dayNumber == today) {
-                        var todayClass = {
-                            name: lessonName,
-                            timeStart: daySnapshot.child('timeStart').val().toString(),
-                            timeEnd: daySnapshot.child('timeEnd').val().toString(),
-                            place: daySnapshot.child('place').val().toString(),
-                            imageUrl:renderLessonImage(lessonName)
+                    //    console.log(daySnapshot.child('timeStart').val().toString())
+                    var todayClass = {
+                        name: lessonName,
+                        timeStart: daySnapshot.child('timeStart').val().toString(),
+                        timeEnd: daySnapshot.child('timeEnd').val().toString(),
+                        place: daySnapshot.child('place').val().toString(),
+                        imageUrl:renderLessonImage(lessonName),
+                        numberOfDay:dayNumber,
+                        day:daySnapshot.key
 
-                        }
-                        classes.push(todayClass);
-                        console.log(classes)
+                    }
+                    dayoflessons.push(todayClass)
+
+                    if(today==6 || today==7){
+
+                       $()
+                        
+                    }
+                    
+                    
+
+                    if (dayNumber == today) {
+
                         if(size>768 && size<1000){
-                            fillCarouselRow(todayClass,carouselInnerElementToday,carouselOuterElementToday,2);
+                            fillCarouselRow(todayClass,'#today_classes_inner','#carousel_today_classes',2);
                         }
                         if(size<768){
-                            fillCarouselRow(todayClass,carouselInnerElementToday,carouselOuterElementToday,1);
+                            fillCarouselRow(todayClass,'#today_classes_inner','#carousel_today_classes',1);
                         }
                         if(size>=1000){
-                            fillCarouselRow(todayClass,carouselInnerElementToday,carouselOuterElementToday,3);
+                            fillCarouselRow(todayClass,'#today_classes_inner','#carousel_today_classes',3);
                         }
 
 
 
                     }
+                    if(dayoflessons.length==scheduleSnapshot.numChildren()){
+                        for(var i=0;i<dayoflessons.length;i++){
+                            //  console.log(dayoflessons[i].name)
+                            if(dayoflessons[i].name=='Business Intelligence'){
+                                console.log('BI',dayoflessons)
+                            }
+                        }
+
+                        var closest=getClosestLesson(dayoflessons,today)
+                        for(var i=0;i<closest.length;i++){
+                            if(size>768 && size<1000){
+                                fillCarouselRow(closest[i],'#next_classes_inner','#carousel_next_classes',2);
+                            }
+                            if(size<768){
+                                fillCarouselRow(closest[i],'#next_classes_inner','#carousel_next_classes',1);
+                            }
+                            if(size>=1000){
+                                fillCarouselRow(closest[i],'#next_classes_inner','#carousel_next_classes',3);
+                            }
+                        }
+                    }
+
+
+
+
+
+
+                    if(size>768 && size<1000){
+                        fillCarouselRow(todayClass,'#week_classes_inner','#carousel_week_classes',2);
+                    }
+                    if(size<768){
+                        fillCarouselRow(todayClass,'#week_classes_inner','#carousel_week_classes',1);
+                    }
+                    if(size>=1000){
+                        fillCarouselRow(todayClass,'#week_classes_inner','#carousel_week_classes',3);
+                    }
+
                 });
+
 
             });
 
@@ -71,10 +131,68 @@ function setUpTodayClasses(size) {
 
 
 }
-function resetIndexes(){
-    carouselNotActiveItemNumber=0;
-    rowIDNumber=0;
+
+function getClosestLesson(lessons,today){
+
+    var arrayForDisplayNextLessons=[]
+    // console.log(lessons)
+    var temp=lessons[0].numberOfDay-today
+    arrayForDisplayNextLessons.push(lessons[0])
+
+    for(var i=1;i<lessons.length;i++){
+        var day=lessons[i]
+        var diff=day.numberOfDay-today
+        if(temp==0){
+            arrayForDisplayNextLessons.pop()
+        }
+        if(diff>=0 && temp>=0 && diff < temp && day.numberOfDay!=today){
+
+            temp=diff;
+            arrayForDisplayNextLessons.pop()
+            arrayForDisplayNextLessons.push(lessons[i])
+
+        }
+
+
+        if(diff<=0 && temp<=0 && diff<temp && day.numberOfDay!=today){
+
+            temp=diff;
+            arrayForDisplayNextLessons.pop()
+            arrayForDisplayNextLessons.push(lessons[i])
+
+        }
+
+    }
+    return arrayForDisplayNextLessons;
+
 }
+
+function shuffleArray(array){
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+
+}
+
+
+
+
+
+
+
 function renderLessonImage(lessonName){
     switch(lessonName){
         case 'Data Mining':
@@ -84,7 +202,25 @@ function renderLessonImage(lessonName){
         case 'Sviluppo di Sistemi Software':
             return 'css/assets/development.svg';
         case 'Applicazioni Web':
-            return 'css/assets/web_development.svg'
+            return 'css/assets/web_development.svg';
+        case 'Big Data':
+            return '';
+        case  'Business Intelligence':
+            return '';
+        case 'Nets Security':
+            return 'css/assets/security.svg';
+        case 'Pervasive Computing':
+            return 'css/assets/pervasive.svg';
+        case 'Distributed Systems':
+            return 'css/assets/distributed_systems.svg';
+        case 'Informative Systems':
+            return '';
+        case 'Smart Robotic Systems':
+            return 'css/assets/robots.svg'
+        case 'Smart City and Mobile Technologies':
+            return 'css/assets/digital-cities.svg';
+        case 'Supporto alle Decisioni':
+            return 'css/assets/supporto_alle_decisioni.svg';
         default: return null;
 
 
@@ -94,59 +230,36 @@ function renderLessonImage(lessonName){
 
 
 
-function fillCarouselRow(todayClass,carouselInnerElement,carouselOuterElement,numberOfImagesPerSlide){
+function fillCarouselRow(todayClass,carouselInnerElementID,carouselOuterElementID,numberOfImagesPerSlide){
 
-    var carouselRow;
     var name=todayClass.name;
     var imageUrl=todayClass.imageUrl;
+    var timeStart=todayClass.timeStart;
+    var timeEnd=todayClass.timeEnd;
+    var place=todayClass.place;
+    var day=todayClass.day;
+    var row
+    var innerElement=$(carouselInnerElementID)
 
+    var elementOfRow='<div class="col"><div class="card" ><img class="card-img-top" src="'+imageUrl+'" alt="Card image cap"><div class="card-body"><h4 class="card-title">'+name+'</h4><p class="card-text">'+timeStart+" "+timeEnd+" "+place+" day: "+day+'</p>';
+    if(innerElement.children().length==0){
+        var carouselItem=$('<div class="carousel-item" id="'+carouselOuterElementID+'" ></div>').appendTo(innerElement)     
+        row=$('<div class="row"></div>').appendTo(carouselItem)        
+        carouselItem.addClass('active')
 
+    }
+    row=innerElement.find('.row').last()
 
-    //    var timeStart=todayClass.timeStart;
-    //    var timeEnd=todayClass.timeEnd;
-    //    var place=todayClass.place;
-
-    /*first item active already in default html template: it gets filled up by max 3 elements
-   */
-    //create new active item
-    if(carouselInnerElement.childElementCount==0){
-        var activeItem='<div class="carousel-item active" id="carousel_item_'+carouselNotActiveItemNumber+'"><div class="row" id="row_'+rowIDNumber+'"></div></div>';
-        carouselInnerElement.insertAdjacentHTML('beforeend',activeItem);    
-
+    if(row.children().length==numberOfImagesPerSlide){
+        carouselItem=$('<div class="carousel-item" id="'+carouselOuterElementID+'" ></div>').appendTo(innerElement)     
+        row=$('<div class="row"></div>').appendTo(carouselItem)
+        var controls=' <a class="carousel-control-prev control" href="'+carouselOuterElementID+'" role="button" data-slide="prev" id="today_carousel_controls"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next control" href="'+carouselOuterElementID+'" role="button" data-slide="next" id="today_carousel_controls"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>'
+        jQuery(carouselOuterElementID).append(controls)
 
 
     }
-    var id=new String("row_"+rowIDNumber)
-    carouselRow=document.getElementById(id);
-    //creo un nuovo item (ma non active)
-
-    if(carouselRow.childElementCount==numberOfImagesPerSlide){
-        rowIDNumber++;
-        carouselNotActiveItemNumber++;
-        var newCarouselItemNotActive='<div class="carousel-item" id="carousel_item_'+carouselNotActiveItemNumber+'" ><div class="row" id="row_'+rowIDNumber+'"></div></div>'
-        carouselInnerElement.insertAdjacentHTML('beforeend',newCarouselItemNotActive);
-        var newId=new String("row_"+rowIDNumber);
-        carouselRow=document.getElementById(newId);
-        var carouselLessonItem='<div class="col"><div class="card" ><img class="card-img-top" src="'+imageUrl+'" alt="Card image cap"><div class="card-body"><h4 class="card-title">'+name+'</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content</p>';
-        carouselRow.insertAdjacentHTML('beforeend',carouselLessonItem);
-
-    }
-
-    else if(carouselRow.childElementCount<numberOfImagesPerSlide){
-        var carouselLessonItem='<div class="col"><div class="card" "><img class="card-img-top" src="'+imageUrl+'" alt="Card image cap"><div class="card-body"><h4 class="card-title">'+name+'</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content</p>';
-
-        carouselRow.insertAdjacentHTML('beforeend',carouselLessonItem);
-
-    }
-
-    if(carouselNotActiveItemNumber==1 && document.getElementById("today_carousel_controls")==null){
-
-        var slideControls=' <a class="carousel-control-prev control" href="#carousel_today_classes" role="button" data-slide="prev" id="today_carousel_controls"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next control" href="#carousel_today_classes" role="button" data-slide="next" id="today_carousel_controls"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
-        carouselOuterElement.insertAdjacentHTML('beforeend',slideControls);
-
-    }
+    row.append(elementOfRow)
 }
-
 function getCarouselItems(carouselInnerElementID){
 
     var rowElements=[];
@@ -160,7 +273,6 @@ function getCarouselItems(carouselInnerElementID){
         })
     })
     rowElements.forEach(function(element){
-        console.log('item ',element);
 
     })
 
@@ -169,20 +281,20 @@ function getCarouselItems(carouselInnerElementID){
 }
 
 
-function resizeAlgorithm(numberOfItemsPerSlide,rowElements,carouselInnerElementID){
-    if(rowElements.length<=numberOfItemsPerSlide){
-        console.log("removing controls",rowElements.length,numberOfItemsPerSlide,document.getElementById('today_carousel_controls'))
-        $('#carousel_today_classes').remove('#today_carousel_controls');
-         console.log(" after removing controls",rowElements.length,numberOfItemsPerSlide,document.getElementById('today_carousel_controls'))
-
-
-
-    }
-    else if(document.getElementById('today_carousel_controls')==null && rowElements.length>numberOfItemsPerSlide){
-        console.log('adding control at number of slide '+numberOfItemsPerSlide)
-        var slideControls=' <a class="carousel-control-prev control" href="#carousel_today_classes" role="button" data-slide="prev" id="today_carousel_controls"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next control" href="#carousel_today_classes" role="button" data-slide="next" id="today_carousel_controls"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
-        $('#carousel_today_classes').append(slideControls);
-    }
+function resizeAlgorithm(numberOfItemsPerSlide,rowElements,carouselInnerElementID,carouselOuterElementID){
+    //    if(rowElements.length<=numberOfItemsPerSlide){
+    //        console.log("removing controls",rowElements.length,numberOfItemsPerSlide,document.getElementById('today_carousel_controls'))
+    //        $(carouselOuterElementID).remove('#today_carousel_controls');
+    //        console.log(" after removing controls",rowElements.length,numberOfItemsPerSlide,document.getElementById('today_carousel_controls'))
+    //
+    //
+    //
+    //    }
+    //    else if(document.getElementById('today_carousel_controls')==null && rowElements.length>numberOfItemsPerSlide){
+    //        console.log('adding control at number of slide '+numberOfItemsPerSlide)
+    //        var slideControls=' <a class="carousel-control-prev control" href="#carousel_today_classes" role="button" data-slide="prev" id="today_carousel_controls"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next control" href="#carousel_today_classes" role="button" data-slide="next" id="today_carousel_controls"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
+    //        $(carouselOuterElementID).append(slideControls);
+    //    }
     $(carouselInnerElementID).empty()
 
     for(var i=0;i<rowElements.length;i++){
@@ -206,6 +318,8 @@ function resizeAlgorithm(numberOfItemsPerSlide,rowElements,carouselInnerElementI
 
 
 }
+
+
 
 
 
