032ed8f22f15cc0fb7cb9ca9c1ddb08870176e1d
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2018-01-25 16:32:19 +0100

    minor

--- js/helprequests.js
+++ js/helprequests.js
@@ -40,19 +40,5 @@ function myFunction() {
     
 }
 
-function createTable(){
-
-
-// Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(0);
-
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell = row.insertCell(0);
-
-// Add some text to the new cells:
-cell.innerHTML = "NEW CELL1";
-
-    
-}
 
 
