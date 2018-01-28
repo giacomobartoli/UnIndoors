59ad381dafd65a52ab5b215f050fcbf662bf1573
GiacomoOracolo <gzano93@gmail.com>
2018-01-29 00:50:06 +0100

    added userstatus background check to helpRequests

--- js/userstatus.js
+++ js/userstatus.js
@@ -6,8 +6,12 @@ var connectedRef = firebase.database().ref(".info/connected");
 connectedRef.on("value", function(snap) {
     if (snap.val() === true) {
         firebase.auth().onAuthStateChanged(function(user){
+
             if(user && user.email.includes('operatore')){
-                firebase.database().ref('operators/'+user.uid+'/status/').set('online')
+
+                firebase.database().ref('operators/'+user.uid+'/status/').set('online').then(function(success){
+            //        alert('value set')
+                })
             }
 
 
@@ -16,6 +20,8 @@ connectedRef.on("value", function(snap) {
         firebase.auth().onAuthStateChanged(function(user){
 
             if(user.email.includes('operatore')){
+             //   alert('offline ' +user.email+" "+user.uid)
+
                 firebase.database().ref('operators/'+user.uid+'/status/').onDisconnect().set('offline')
             }
 
