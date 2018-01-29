0b09b4399fa7f3edf85406d57d0d5d2af6428529
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2018-01-29 14:00:05 +0100

    restored

--- js/helprequests.js
+++ js/helprequests.js
@@ -13,7 +13,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var table=document.getElementById('myTable');
 
-function myFunction() {
+function myFunction(filter) {
                   
     
      database.ref('helprequests/').on('value', function (snapshot) {
@@ -35,9 +35,13 @@ function myFunction() {
             var childSnapshotIndex=childSnapshot.key
             //Using this alert to check if values are retrieved correctly
             //alert(from);
-            
+            if(filter=='yes'){
+                if(status!='accepted'){
             tbody.append('<tr><th scope="row">'+id+'</th><td>'+from+'</td><td>'+status+'</td><td>'+msg+'</td><td>'+place+'</td><td><button type="button" class="btn btn-outline-success" id="'+count+'">Accept</button><button type="button" class="btn btn-outline-danger" id="'+count+'R'+'">Refuse</button></td></tr>')
-            
+                }
+                }else{
+                 tbody.append('<tr><th scope="row">'+id+'</th><td>'+from+'</td><td>'+status+'</td><td>'+msg+'</td><td>'+place+'</td><td><button type="button" class="btn btn-outline-success" id="'+count+'">Accept</button><button type="button" class="btn btn-outline-danger" id="'+count+'R'+'">Refuse</button></td></tr>')
+            }
             $('#'+count).click(function(){
                 setRequests(id,childSnapshotIndex,$(this).attr('id'),index)
                 
@@ -67,59 +71,6 @@ function myFunction() {
 
 }
                                                       
-
-function filterTable(){
-    
-    
-       database.ref('helprequests/').on('value', function (snapshot) {
-         
-         var tbody=$('#table_body')
-         tbody.empty();
-        //qui ho tutto
-            var count=0
-            var status = snapshot.val();
-       // var tbody = document.querySelector("#myTable tbody");
-            
-            snapshot.forEach(function (childSnapshot) {
-            var id = childSnapshot.child('id').val().toString();
-            var msg = childSnapshot.child('message').val().toString();
-            var from = childSnapshot.child('name').val().toString();
-            var status = childSnapshot.child('state').val().toString();
-            var place = childSnapshot.child('place').val().toString();
-            var index = childSnapshot.child('index').val().toString();
-            var childSnapshotIndex=childSnapshot.key
-            //Using this alert to check if values are retrieved correctly
-            //alert(from);
-            if(status!='accepted'){
-            tbody.append('<tr><th scope="row">'+id+'</th><td>'+from+'</td><td>'+status+'</td><td>'+msg+'</td><td>'+place+'</td><td><button type="button" class="btn btn-outline-success" id="'+count+'">Accept</button><button type="button" class="btn btn-outline-danger" id="'+count+'R'+'">Refuse</button></td></tr>')
-            }
-            $('#'+count).click(function(){
-                setRequests(id,childSnapshotIndex,$(this).attr('id'),index)
-                
-               
-            })
-            $('#'+count+'R').click(function(){
-                setRequestDenied(id,childSnapshotIndex,$(this).attr('id'),index)
-            })
-            
-            database.ref('helprequests/'+childSnapshotIndex+'/state/').on('value',  snapshot => {
-                
-                    var requestStatus = snapshot.val().toString()
-                    if(requestStatus == 'accepted'){
-                        var idButton = count;
-                        
-                        $('#'+idButton).attr("disabled","");
-                        $('#'+idButton+'R').attr("disabled","");
-                    }
-                })    
-                
-                
-            count++
-                
-    })
-    
-}) 
-}
 
 
 function setRequests(id,childSnapshotIndex,butcount,index){
