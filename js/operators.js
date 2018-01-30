336bc1bad7b2da2a5637b241bd7909959124bdda
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2018-01-30 22:17:28 +0100

    requests badge

--- js/operators.js
+++ js/operators.js
@@ -1,4 +1,5 @@
 var count = 0;
+var req = 0;
 
 function countOperators(){
     
@@ -16,3 +17,29 @@ function updateLabel(count){
     //alert(''+count)
     $('#myBadge').text(''+count)
 }
+
+function updateLabelreq(req){
+    //alert(''+count)
+    $('#myBadgeReq').text(''+req)
+}
+
+
+function countRequests(){
+    
+    
+     var connectedRef=database.ref('helprequests/').on('value', function (snapshot) {
+         
+         snapshot.forEach(function (childSnapshot) {
+          var status = childSnapshot.child('state').val().toString();
+            if(status=='pending'){
+                req++;
+                updateLabelreq(req)
+            }
+         }
+     );
+    
+
+});
+}
+                                                       
+                                                       
