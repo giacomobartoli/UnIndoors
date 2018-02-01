a7fdb233116ce63dfc32fe7edbe5c9d65afc4674
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2018-02-01 18:15:50 +0100

    FUCKING MODALS FIXED!!!

--- js/helprequests.js
+++ js/helprequests.js
@@ -15,8 +15,6 @@ var table=document.getElementById('myTable');
 
 function myFunction(filter) {
 
-
-
     database.ref('helprequests/').on('value', function (snapshot) {
 
         var tbody=$('#table_body')
@@ -24,6 +22,7 @@ function myFunction(filter) {
         //qui ho tutto
         var count=0
         var status = snapshot.val();
+        var container=$('#table_container') 
         // var tbody = document.querySelector("#myTable tbody");
 
         snapshot.forEach(function (childSnapshot) {
@@ -34,38 +33,29 @@ function myFunction(filter) {
             var place = childSnapshot.child('place').val().toString();
             var index = childSnapshot.child('index').val().toString();
             var childSnapshotIndex=childSnapshot.key
+
             //Using this alert to check if values are retrieved correctly
             //alert(from);
+            
             if(filter=='yes'){
                 if(status!='accepted'){
                     tbody.append('<tr><th scope="row">'+id+'</th><td>'+from+'</td><td>'+status+'</td><td>'+msg+'</td><td>'+place+'</td><td><button type="button" class="btn btn-outline-success" id="'+count+'">Accept</button><button type="button" class="btn btn-outline-danger" id="'+count+'R'+'">Refuse</button></td></tr>')
                 }
             }else{
-                tbody.append('<tr><th scope="row">'+id+'</th><td>'+from+'</td><td>'+status+'</td><td>'+msg+'</td><td>'+place+'</td><td><button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal_'+count+'" id="'+count+'">Accept</button><button type="button" class="btn btn-outline-danger" id="'+count+'R'+'">Refuse</button></td></tr>')
-                var container=$('#table_container')                
-                container.append('<div class="modal fade" id="myModal_'+count+'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLongTitle">Reply to student:</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><form><div class="form-group"><label for="message-text" class="col-form-label">Message:</label><textarea class="form-control" id="message-text"></textarea></div></form></div><div class="modal-footer"><button type="button" class="btn btn-primary" id="'+count+'REP'+'" >Send Message</button></div></div></div></div>')
+
+                     tbody.append('<tr><th scope="row">'+id+'</th><td>'+from+'</td><td>'+status+'</td><td>'+msg+'</td><td>'+place+'</td><td><button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" id="'+count+'">Accept</button><button type="button" class="btn btn-outline-danger" id="'+count+'R'+'">Refuse</button></td></tr>')
+
             }
+                               
+            
             $('#'+count).click(function(){
-                setRequests(id,childSnapshotIndex,$(this).attr('id'),index)
-                //$('#myModal_'+$(this).attr('id')).modal('show');
-
-            })
-            $('#'+count+'REP').click(function(){
             
-            var string=$(this).attr('id').toString(); //0REP
-            var string2=string.slice(0,-3) //0
-            var modal_to_close='#myModal_'+string2+'' //#myModal0
-                
-            var msg = document.getElementById('message-text').value
-            setReplyMsg(id,msg,index)
+            setRequests(id,childSnapshotIndex,$(this).attr('id'),index)
+
+            })
+
             
-            $(modal_to_close).modal('hide')
-
-            })
-            $('#myModal').on('shown.bs.modal', function () {
-                $('#myInput').trigger('focus')
-
-            })
+            
             $('#'+count+'R').click(function(){
                 setRequestDenied(id,childSnapshotIndex,$(this).attr('id'),index)
             })
@@ -80,8 +70,6 @@ function myFunction(filter) {
                     $('#'+idButton+'R').attr("disabled","");
                 }
             })    
-
-
 
             count++
 
@@ -100,6 +88,7 @@ function setRequests(id,childSnapshotIndex,butcount,index){
         $('#'+idButton).attr("disabled","");
         $('#'+idButton+'R').attr("disabled","");
     });
+    
     updateRequestForStudent(id,'accepted',index)
 }
 
@@ -114,11 +103,28 @@ function setRequestDenied(id,childSnapshotIndex,count,index){
 }
 
 function setReplyMsg(idReq,msg,index){
-    database.ref('users/'+idReq+'/helprequests/'+index+'/operator_message/').set(msg)
+    
+    alert('msg: '+msg+'  idReq: '+idReq+'  index: '+index)
+    database.ref('users/'+idReq+'/helprequests/'+index+'/operator_message/').set(msg).then(sucess=>{
+        
+        $('#REP').unbind('click')
+        
+    })
 }
 
 function updateRequestForStudent(idReq,stateToSet,index){
-    database.ref('users/'+idReq+'/helprequests/'+index+'/state/').set(stateToSet)
+    database.ref('users/'+idReq+'/helprequests/'+index+'/state/').set(stateToSet).then(success=>{
+            $('#REP').click(function(){
+
+            var msg = document.getElementById('MERDA').value
+           // alert(stampa)
+            setReplyMsg(idReq,msg,index)
+
+            
+            $('#myModal').modal('hide')
+
+            })
+    })
 }
 
 
